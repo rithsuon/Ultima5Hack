d551b0340a93692f4790d165cf69f7ad4db5a39d
rithsuon <riths2013@gmail.com>
2021-03-21 04:05:44 -0700

    finished stats menu for player and npcs

--- src/UltimaVHack.java
+++ src/UltimaVHack.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 
 public class UltimaVHack {
@@ -24,24 +25,34 @@ public class UltimaVHack {
             {"1D2", "1D4", "1CE", "1CF", "1D0", "1D6"}, //13: Toshi
             {"1F2", "1F4", "1EE", "1EF", "1F0", "1F6"}, //14: Saduj
     };
-    private static final int MAX_SHORT_VAL = 65536;// max unsigned int value for a short
-
+    private static final int MAX_SHORT_VAL = 65536;// range of unsigned int value for a short
+    private static Scanner input = new Scanner(System.in);
     private static RandomAccessFile file;
 
     public static void main(String[] args) {
         try {
             file = new RandomAccessFile("c:/Users/Rith/Documents/dos/Ultima_5/Ultima_5/SAVED.GAM", "rw");
-            long offset = Long.parseLong("240", 16);
-            file.seek(Long.parseLong(playerStatsOffsets[0], 16));
-
-            safeWrite( checkShort(599), npcStatsOffsets[0][5]);
-            safeWrite( checkShort(100), npcStatsOffsets[2][5]);
+            boolean inUse = true;
+            while(inUse) {
+                System.out.println("Select a character to edit:\n1) Player\n2) Shamino\n3) Iolo\n4) Mariah\n5) Geoffrey" +
+                        "\n6) Jaana\n7) Julia\n8) Dupre\n9) Katrina\n10) Sentri\n11) Johne\n12) Gorn\n13) Maxwell\n14) Toshi\n15) Saduj\n-1) Save changes and quit.");
+                int choice = validateInt(input);
+                if(choice == -1) {
+                    inUse = false;
+                } else if (choice == 1) {
+                    playerMenu();
+                } else if (choice >= 2 && choice <= 15) {
+                    statsMenu(choice-2);
+                } else {
+                    System.out.println("Invalid Choice.");
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                System.out.println("");
+                System.out.println("Saving changes and quitting...");
                 file.close();
             } catch (IOException i) {
                 i.printStackTrace();
@@ -49,12 +60,57 @@ public class UltimaVHack {
         }
     }
 
+    private static void playerMenu() {
+        while(true) {
+            System.out.println("Select an option:\n1) Edit player items\n2) Edit player stats");
+            int choice = validateInt(input);
+            if(choice == 1) {
+                break;
+            } else if (choice == 2) {
+                statsMenu(-1);
+                break;
+            } else {
+                System.out.println("Invalid Choice.");
+            }
+        }
+    }
+
+    private static void statsMenu(int charIdx) {
+        String offset;
+        int choice = 0;
+
+        while(true) {
+            System.out.println("Pick a stat to edit:\n0) HP\n1) Max HP\n2) Strength\n3) Dexterity\n4) Intellect\n5) Experience");
+             choice = validateInt(input);
+             if(choice >= 0 && choice <= 5) {
+                 break;
+             } else {
+                 System.out.println("Invalid Choice.");
+             }
+        }
+
+        System.out.println("Enter new stat value: ");
+        int statVal = validateInt(input);
+
+        if(charIdx < 0) {
+            offset = playerStatsOffsets[choice];
+        } else {
+            offset = npcStatsOffsets[charIdx][choice];
+        }
+
+        if(choice == 0 || choice == 1 || choice == 5) {
+            safeWriteShort(statVal, offset);
+        } else {
+            safeWriteByte(statVal, offset);
+        }
+    }
 
     private static int checkShort(int val) {
         return val % MAX_SHORT_VAL;
     }
 
-    private static void safeWrite(int value, String offset) {
+    private static void safeWriteShort(int value, String offset) {
+        value = checkShort(value); //verify value is within range of 2 bytes representation
         String hexStr = Integer.toHexString(value);
         long offsetVal = Long.parseLong(offset, 16);
         try {
@@ -71,6 +127,24 @@ public class UltimaVHack {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void safeWriteByte(int value, String offset) {
+        long offsetVal = Long.parseLong(offset, 16);
+        try {
+            file.seek(offsetVal);
+            file.writeByte(value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int validateInt(Scanner userInput) {
+        while(!userInput.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a number.");
+            userInput.next();
+        }
+        return userInput.nextInt();
     }
 
     private static String removeTrailingZeroes(String s) {
